025ac9ca4da3b9d2b7496b75a0ba257af9ff099d
ediaz <ediaz@192.168.0.31>
2021-03-22 20:59:01 -0600

    Version 1

--- src/org/biblioteca/control/MuestraBuscar.java
+++ src/org/biblioteca/control/MuestraBuscar.java
@@ -1,14 +1,43 @@
 package org.biblioteca.control;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.biblioteca.interfaces.Buscar;
 import org.biblioteca.interfaces.Libro;
 
 public class MuestraBuscar implements Buscar{
+    private List<Object> libros;
+    
+    public MuestraBuscar() {
+        libros = new ArrayList<Object>();
+        creaLista();
+    }
 
+    private void creaLista(){
+        libros.add(new MuestraLibro(152,"El Llano en llamas","Compilacion de cuentos escritos por Juan Rulfo",150));
+        libros.add(new MuestraLibro(153,"Pedro Páramo","Primera novela del escritor mexicano Juan Rulfo",55));
+        libros.add(new MuestraLibro(154,"Como agua para chocolate","Novela romántica escrita por Laura Esquivel",60));
+        libros.add(new MuestraLibro(155,"El laberinto de la soledad","Ensayo publicado por el escritor mexicano Octavio Paz",20));
+        libros.add(new MuestraLibro(156,"Las batallas en el desierto","Novela corta de José Emilio Pacheco",15));
+        libros.add(new MuestraLibro(157,"Confabulario","Sucesivas recopilaciones de relatos escritos por Juan José Arreola",200));
+        libros.add(new MuestraLibro(158,"La muerte de Artemio Cruz","Novela del escritor mexicano Carlos Fuentes",37));
+    }
+    
     @Override
     public List<Libro> buscar(String... palabrasClaves) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Libro> resultados = new ArrayList<Libro>();
+        
+        for (Object ml : libros){
+            if ( palabrasClaves[0].equals( ((MuestraLibro)ml).getTitulo() ) ){
+                resultados.add((MuestraLibro)ml);
+            }
+        }
+        
+        if (resultados != null && !resultados.isEmpty()) { 
+            return resultados;
+        }
+        
+        throw new Exception("Excepcion: Libro no encontrado.");
     }
     
 }
--- src/org/biblioteca/test/Test.java
+++ src/org/biblioteca/test/Test.java
@@ -1,5 +1,26 @@
 package org.biblioteca.test;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.biblioteca.control.MuestraBuscar;
+import org.biblioteca.control.MuestraLibro;
+import org.biblioteca.interfaces.Libro;
+
 public class Test {
-    
+    public static void main(String[] args){
+        MuestraBuscar mb = new MuestraBuscar();
+        List<Libro> libros = null;
+        
+        try {
+            libros = mb.buscar("El Llano en llamas");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        if (libros != null && !libros.isEmpty()) { 
+            for (Libro temp : libros) {
+                System.out.println( ((MuestraLibro)temp).getInformacion() );
+            }
+        }
+    }
 }
