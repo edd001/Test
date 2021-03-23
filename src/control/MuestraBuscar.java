3d08a1f113d8ab14571a22186c2811aca48e8691
ediaz <>
2021-03-22 18:43:38 -0600

    Version 1

---
+++ src/control/MuestraBuscar.java
@@ -0,0 +1,14 @@
+package control;
+
+import java.util.List;
+import org.biblioteca.interfaces.Buscar;
+import org.biblioteca.interfaces.Libro;
+
+public class MuestraBuscar implements Buscar{
+
+    @Override
+    public List<Libro> buscar(String... strings) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
