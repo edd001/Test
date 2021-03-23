bc0a9ef8b96cf63359ad1b300045a4a3bfcafe90
ediaz <>
2021-03-23 13:32:36 -0600

    Version 1

--- src/org/biblioteca/control/MuestraBuscar.java
+++ src/org/biblioteca/control/MuestraBuscar.java
@@ -9,18 +9,18 @@ public class MuestraBuscar implements Buscar{
     private List<Object> libros;
     
     public MuestraBuscar() {
-        libros = new ArrayList<Object>();
+        this.libros = new ArrayList<Object>();
         creaLista();
     }
 
     private void creaLista(){
-        libros.add(new MuestraLibro(152,"El Llano en llamas","Compilacion de cuentos escritos por Juan Rulfo",150));
-        libros.add(new MuestraLibro(153,"Pedro Páramo","Primera novela del escritor mexicano Juan Rulfo",55));
-        libros.add(new MuestraLibro(154,"Como agua para chocolate","Novela romántica escrita por Laura Esquivel",60));
-        libros.add(new MuestraLibro(155,"El laberinto de la soledad","Ensayo publicado por el escritor mexicano Octavio Paz",20));
-        libros.add(new MuestraLibro(156,"Las batallas en el desierto","Novela corta de José Emilio Pacheco",15));
-        libros.add(new MuestraLibro(157,"Confabulario","Sucesivas recopilaciones de relatos escritos por Juan José Arreola",200));
-        libros.add(new MuestraLibro(158,"La muerte de Artemio Cruz","Novela del escritor mexicano Carlos Fuentes",37));
+        this.libros.add(new MuestraLibro(152,"El Llano en llamas","Compilacion de cuentos escritos por Juan Rulfo",150));
+        this.libros.add(new MuestraLibro(153,"Pedro Páramo","Primera novela del escritor mexicano Juan Rulfo",55));
+        this.libros.add(new MuestraLibro(154,"Como agua para chocolate","Novela romántica escrita por Laura Esquivel",60));
+        this.libros.add(new MuestraLibro(155,"El laberinto de la soledad","Ensayo publicado por el escritor mexicano Octavio Paz",20));
+        this.libros.add(new MuestraLibro(156,"Las batallas en el desierto","Novela corta de José Emilio Pacheco",15));
+        this.libros.add(new MuestraLibro(157,"Confabulario","Sucesivas recopilaciones de relatos escritos por Juan José Arreola",200));
+        this.libros.add(new MuestraLibro(158,"La muerte de Artemio Cruz","Novela del escritor mexicano Carlos Fuentes",37));
     }
     
     @Override
@@ -29,7 +29,7 @@ public class MuestraBuscar implements Buscar{
         
         for (Object ml : libros){
             if ( palabrasClaves[0].equals( ((MuestraLibro)ml).getTitulo() ) ){
-                resultados.add((MuestraLibro)ml);
+                resultados.add((Libro)ml);
             }
         }
         
--- src/org/biblioteca/test/Test.java
+++ src/org/biblioteca/test/Test.java
@@ -1,19 +1,24 @@
 package org.biblioteca.test;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.biblioteca.control.MuestraBuscar;
 import org.biblioteca.control.MuestraLibro;
 import org.biblioteca.interfaces.Libro;
 
 public class Test {
+    private static int CLAVE = 0;
+    private static int TITULO = 1;
+    private static int DESCRIPCION = 2;
+    
     public static void main(String[] args){
+        
+        String[] cvebusca = {Integer.toString(CLAVE), Integer.toString(TITULO), Integer.toString(DESCRIPCION)}; 
         MuestraBuscar mb = new MuestraBuscar();
         List<Libro> libros = null;
         
         try {
-            libros = mb.buscar("El Llano en llamas");
+            libros = mb.buscar("0","Llanoes");
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
